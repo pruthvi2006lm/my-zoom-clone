e7bf5fd5102f64f08560a8986764c3189db8317c
pruthvi2006lm <pk22pruthvi@gmail.com>
2025-02-11 20:01:24 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_STREAM_API_KEY: process.env.NEXT_PUBLIC_STREAM_API_KEY,
+  },
+};
 
 export default nextConfig;
